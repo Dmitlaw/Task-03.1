81d0f7ff01cc23d7e5d8c0a8611bdc0ca5071338
Dmitlaw <dmitpechagin@yandex.ru>
2025-10-22 23:17:47 +0300

    second commit

--- main.cpp
+++ main.cpp
@@ -2,37 +2,38 @@
 
 class Calculator {
 
-public:
+private:
     double num1{};
     double num2{};
 
+public:
     Calculator() = default;
 
-    double add(double num1, double num2) {
+    double add() const {
         return num1 + num2;
     }
 
-    double multiply(double num1, double num2) {
+    double multiply() const {
         return num1 * num2;
     }
 
-    double subtract_1_2(double num1, double num2) {
+    double subtract_1_2() const {
         return num1 - num2;
     }
 
-    double subtract_2_1(double num1, double num2) {
+    double subtract_2_1() const {
         return num2 - num1;
     }
 
-    double divide_1_2(double num1, double num2) {
+    double divide_1_2() const {
         return num1 / num2;
     }
 
-    double divide_2_1(double num1, double num2) {
+    double divide_2_1() const {
         return num2 / num1;
     }
 
-    bool set_num1(double num1) {
+    bool set_num1() {
 
         if (num1 != 0) {
             this->num1 = num1;
@@ -43,7 +44,7 @@ public:
         }
     }
 
-    bool set_num2(double num2) {
+    bool set_num2() {
 
         if (num2 != 0) {
             this->num2 = num2;
@@ -52,6 +53,14 @@ public:
         else {
             return false;
         }
+    }
+
+    void set_num1(double num1) {
+        this->num1 = num1;
+    }
+
+    void set_num2(double num1) {
+        this->num2 = num1;
     }
 };
 
@@ -64,25 +73,30 @@ int main() {
     while (true) {
 
         while (true) {
-            if (num1 == 0) { std::cout << "Неверный ввод!" << std::endl; }
             std::cout << "Введите num1: ";
             std::cin >> num1;
-            if (calculator.set_num1(num1)) { break; }
+            calculator.set_num1(num1);
+            if (calculator.set_num1()) {
+                break;
+            }
+            std::cout << "Неверный ввод!" << std::endl;
         }
 
         while (true) {
-            if (num2 == 0) { std::cout << "Неверный ввод!" << std::endl; }
             std::cout << "Введите num2: ";
             std::cin >> num2;
-            if (calculator.set_num2(num2)) { break; }
+            calculator.set_num2(num2);
+            if (calculator.set_num2()) {
+                break;
+            }
+            std::cout << "Неверный ввод!" << std::endl;
         }
 
-        std::cout << "num1 + num2 = " << calculator.add(num1, num2) << std::endl;
-        std::cout << "num1 * num2 = " << calculator.multiply(num1, num2) << std::endl;
-        std::cout << "num1 - num2 = " << calculator.subtract_1_2(num1, num2) << std::endl;
-        std::cout << "num2 - num1 = " << calculator.subtract_2_1(num1, num2) << std::endl;
-        std::cout << "num1 / num2 = " << calculator.divide_1_2(num1, num2) << std::endl;
-        std::cout << "num2 / num1 = " << calculator.divide_2_1(num1, num2) << std::endl;
+        std::cout << "num1 + num2 = " << calculator.add() << std::endl;
+        std::cout << "num1 * num2 = " << calculator.multiply() << std::endl;
+        std::cout << "num1 - num2 = " << calculator.subtract_1_2() << std::endl;
+        std::cout << "num2 - num1 = " << calculator.subtract_2_1() << std::endl;
+        std::cout << "num1 / num2 = " << calculator.divide_1_2() << std::endl;
+        std::cout << "num2 / num1 = " << calculator.divide_2_1() << std::endl;
     }
-
 }
